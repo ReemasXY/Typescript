87acee269a675066ce562621b14129df4ec5652b
ReemasXY <sameermhr345@gmail.com>
2026-04-12 18:57:38 +0545

    Typescript started

---
+++ Introduction_Typescript/intro.js
@@ -0,0 +1,4 @@
+"use strict";
+let user = { name: "Sameer", age: 10 };
+console.log("sameer");
+console.log(user.email);
---
+++ Introduction_Typescript/intro.ts
@@ -0,0 +1,5 @@
+let user={name:"Sameer", age: 10};
+
+console.log("sameer");
+console.log(user.email);
+
