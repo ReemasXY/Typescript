07fcc11812c5c6eeb170ffb7f892b81f556d0daa
ReemasXY <sameermhr345@gmail.com>
2026-05-07 18:48:54 +0545

    undersatanding concept of union and any

---
+++ Types_in_TypesScript/src/union&and.ts
@@ -0,0 +1,22 @@
+//union means | in ts
+//for example
+let a: number | string; // yesma aba a ma ki number ki string duitai rakhna milxa bhana khojeko ho
+// tara SAKESAMMA AVOID USING UNION TYPES
+
+// unions ma just default datatype matra haina ki custom type ya value ni rakhna milxa
+let b: "sameer" | "suman"; // yesma aba b ma ki sameer ki suman rakhna milxa bhana khojeko ho
+// yesma aba b ma sameer ya suman matra rakhna milxa 
+// aru rakhna mildaina
+
+// let c: "minisha"= "sammer"// yesto garna milena
+let c: "minisha"
+c= "minisha" // yesma aba c ma minisha matra rakhna milxa
+
+//ANY any bhanna le variable ma j sukai type ko value rakhna milxa
+let d; // yo bhaneko any infer garxa yesle default kinaki thanaixaina k auxa bhanera
+d= 10; // d ma number rakhna milxa
+d= "sameer"; // d ma string rakhna milxa
+//yesto kei error audaina 
+
+
+// tara sakesamma union ra any use garnu hudaina natra typescript use gareko sense nai banaudaina
