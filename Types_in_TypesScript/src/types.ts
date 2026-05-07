30fd3a7fb6db56173854fd4df5ab92924f0b5132
ReemasXY <sameermhr345@gmail.com>
2026-05-07 18:30:23 +0545

    completed type inferencing and annotations

---
+++ Types_in_TypesScript/src/types.ts
@@ -0,0 +1,10 @@
+//type inferencing
+// yesle chai afai bujxa ki variable kun type ko ho
+//explicitly bhannu pardaina 
+let a = 10; // a variable ma aba number matra rakhna milxa bhanera auto bujxa
+// a= "sameer"  yo garna payena aba error dinxa 
+
+//type annotation
+// yeslai chai explicitly bhannu parxa ki variable ko type kun ho
+let b: string = "sameer"; // b variable ma string matra rakhna milxa bhanera explicitly bhanna parxa
+// b= 10;  yo garna payena aba error dinxa
